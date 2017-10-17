177e4f72ddd7b45c9f48b6ce776441652a0fa103
brunzero <brunothebeast@gmail.com>
2017-10-17 15:40:50 -0400

    get rid of extraneous styling, added a nav bar

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,16 +7,30 @@ if(process.env.BROWSER)
 class Home extends React.Component {
   render() {
     return (
-      <div className="home-wrapper columns">
-        <div className="column">
-          <div className="card">
-            <div className="content">
-              BLABLLALSDLASDLASLDLASL
+      <div className="home-wrapper">
+        <nav className="navbar" role="navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+              <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox"/>
+            </a>
+            <div className="navbar-burger">
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
           </div>
+        </nav>
+        <div className="columns">
+          <div className="column">
+            <div className="card">
+              <div className="content">
+                BLABLLALSDLASDLASLDLASL
+              </div>
+            </div>
+          </div>
+          <div className="column">This</div>
+          <div className="column">A Column</div>
         </div>
-        <div className="column">This</div>
-        <div className="column">A Column</div>
       </div>
     );
   }
