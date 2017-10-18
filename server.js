726fefc2db3a45e5969ad36a7504896e0ffb23df
brunzero <brunothebeast@gmail.com>
2017-10-18 17:51:25 -0400

    Revert "cross origin request stuff"
    This reverts commit 1efef57d214e1b99b015731f167accff777e2045.

--- server.js
+++ server.js
@@ -98,13 +98,6 @@ app.use(function(req, res, next) {
   }
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
 if (app.get('env') === 'development') {
   app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
