1e8b20f846a562937fe69d4d19e52703637ceb7a
brunzero <brunothebeast@gmail.com>
2017-10-18 02:09:19 -0400

    get rid of react-transform-hmr in prod

--- webpack.config.js
+++ webpack.config.js
@@ -102,10 +102,6 @@ else{
               ['react-transform', {
                 transforms: [
                   {
-                    transform: 'react-transform-hmr',
-                    imports: ['react'],
-                    locals: ['module']
-                  }, {
                     transform: 'react-transform-catch-errors',
                     imports: ['react', 'redbox-react']
                   }
