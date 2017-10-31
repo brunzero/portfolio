1bc6b11c20ab209cf75eafbeaca296ac86b819d6
brunzero <brunothebeast@gmail.com>
2017-10-30 20:39:54 -0400

    conditional rendering for chapters

--- app/components/Reader.js
+++ app/components/Reader.js
@@ -22,8 +22,10 @@ class Reader extends React.Component {
       chaptertitle: "",
       chapternum: 1,
       pagenum: 1,
+      success: true
     };
     this.requestChapter = this.requestChapter.bind(this);
+    this.renderChapter = this.renderChapter.bind(this);
   }
   componentDidMount(){
   }
@@ -32,9 +34,9 @@ class Reader extends React.Component {
     var title = title.replace(/[ \t]+$/, '');
     title = title.replace(/ /g, '-').toLowerCase();
     title = title.replace(/[^\w-]/g,'');
-    self.setState({loading: true});
     fetch(requestParser.uriMinusPath+'/chapter/'+title+'/'+chapternum)
       .then(function(response){
+        self.setState({loading:false})
         return response.json()
       })
       .then(function(response){
@@ -46,16 +48,55 @@ class Reader extends React.Component {
             chapter: response.pages,
             chaptertitle: response.name,
             chapternum: parseInt(chapternum),
-            loading: false
+            success: true
           });
+        }
+        else{
+          self.setState({success: false})
         }
       })
   }
   submitForm(e){
     e.preventDefault();
-    var title = this.refs.title.value;
-    var chapter = this.refs.chapter.value;
+    var title = this.refs.title.value || "";
+    var chapter = this.refs.chapter.value || 1;
     this.requestChapter(title, chapter);
+  }
+  renderChapter(){
+    const self = this;
+    var chapters;
+    if(this.state.loading){
+      return (
+        <div>
+          <figure className="loading-wrapper">
+            <div className="loading"/>
+          </figure>
+        </div>
+      )
+    }
+    else if(!this.state.success){
+      return(
+        <div className="text">
+          <span className="notfound">I'm sorry that manga could not be found</span>
+        </div>
+      )
+    }
+    else if(!this.state.loading && this.state.success && this.state.chapter.length>0){
+      chapters = this.state.chapter.map(function(page, i){
+        var url = page.url.replace(/^http:\/\//i, 'https://');
+        return (
+          <img className={`chapter-${self.state.chapternum} page-${page.pageId}`} src={url} key={i}/>
+        )
+      })
+      return chapters;
+    }
+    else{
+      return(
+        <div className="text">
+          <span>The result of your search goes here!</span>
+        </div>
+      )
+    }
   }
   render(){
     let title = this.state.title || "";
@@ -66,36 +107,24 @@ class Reader extends React.Component {
     return(
       <div className="reader-wrapper">
         <Header size="small" color="palette1" textcolor="white" reader/>
-          <form className = "form field" onSubmit={(e)=>{this.submitForm(e)}}>
-            <div className = "manga">
-              <label className="label">Manga Title</label>
-              <div className = "control"> 
-                <input ref="title" className="input" type="text" placeholder="one piece"/>
-              </div>
+        <form className = "form field" onSubmit={(e)=>{this.submitForm(e)}}>
+          <div className = "manga">
+            <label className="label">Manga Title</label>
+            <div className = "control"> 
+              <input ref="title" className="input" type="text" placeholder="one piece"/>
             </div>
-            <div className = "chapter">
-              <label className="label">Ch</label>
-              <div className = "control"> 
-                <input ref="chapter" className="input" type="text" placeholder="1"/>
-              </div>
+          </div>
+          <div className = "chapter">
+            <label className="label">Ch</label>
+            <div className = "control"> 
+              <input ref="chapter" className="input" type="text" placeholder="1"/>
             </div>
-            <input className="button" type="submit"/>
-          </form>
+          </div>
+          <input className="button" type="submit"/>
+        </form>
         <div className="chapter-wrapper">
-          <Hero color="palette1" title={title} subtitle={"Chapter " + chapternum + ": " + chaptertitle} textcolor="palette4" subtextcolor="white" centered = "centered">    
-              {!this.state.loading ? chapter.map(function(page, i){
-                var url = page.url.replace(/^http:\/\//i, 'https://');
-                  return (
-                    <img className={`chapter-${chapternum} page-${page.pageId}`} src={url} key={i}/>
-                  )
-                })
-                :
-                <div>
-                  <figure className="loading-wrapper">
-                    <div className="loading"/>
-                  </figure>
-                </div>
-              }
+          <Hero color="palette1" title={title} subtitle={"Chapter " + chapternum + ": " + chaptertitle} textcolor="palette4" subtextcolor="white" centered = "centered"> 
+            {this.renderChapter()}   
           </Hero>
           <div className="left-button" onClick={()=>this.requestChapter(title, (chapternum-1))}>
             <SVG name="leftarrow"/>
