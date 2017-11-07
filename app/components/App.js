de6ab1c1e8886b853c9f67a48ac6d59bea743cbb
brunzero <brunothebeast@gmail.com>
2017-11-07 02:26:46 -0500

    added spotify player

--- app/components/App.js
+++ app/components/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
     return (
       <div className="app">
         {this.props.children}
+        <div className="music-player">
+          <iframe src="https://open.spotify.com/embed?uri=spotify:user:12129645831:playlist:0hhpEDlyBgujrGy7T0c4uK" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>        
+        </div>
       </div>
     );
   }
